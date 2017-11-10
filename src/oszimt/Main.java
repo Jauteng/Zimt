540d3b5b93842d326d39f4be58cd234306893fdf
he-mat <hegel@pfefferwerk.de>
2017-11-10 14:57:55 +0100

    added icon

--- src/oszimt/Main.java
+++ src/oszimt/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,6 +15,7 @@ public class Main extends Application {
         primaryStage.setTitle("ZIMT GmbH & Co KG. - login");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+        primaryStage.getIcons().add(new Image("/oszimt/icon128.png"));
     }
 
 
